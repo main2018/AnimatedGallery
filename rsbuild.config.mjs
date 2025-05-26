d011df87f71a3ff99a85caa63b9c448e34a97bd6
wangpan <pan.wang@ushow.media>
2025-05-26 18:21:32 +0800

    init

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,3 +1,11 @@
+/*
+ * @Author: wangpan pan.wang@ushow.media
+ * @Date: 2025-05-26 17:29:11
+ * @LastEditors: wangpan pan.wang@ushow.media
+ * @LastEditTime: 2025-05-26 18:20:38
+ * @FilePath: /AnimatedGallery/rsbuild.config.mjs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
@@ -5,5 +13,11 @@ export default defineConfig({
   plugins: [pluginReact()],
   html: {
     template: './public/index.html',
+  },
+  output: {
+    distPath: {
+      root: './dist',
+    },
+    assetPrefix: '/gallery/',
   },
 });
